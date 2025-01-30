97834b5f0a63edefdddc23163f071056b62e47a7
StephanyeCunto <stephanyecristine6@gmail.com>
2025-01-30 10:20:02 -0300

    Criação da interface

---
+++ src/main/java/com/App.java
@@ -0,0 +1,34 @@
+package com;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class App extends Application {
+    private static Scene scene;
+    @Override
+    public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
+        stage.setTitle("paint Brush");
+        scene = new Scene(loadFXML("paintbrush"));
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+   public static void main(String[] args) {
+        launch();
+    }
+}
